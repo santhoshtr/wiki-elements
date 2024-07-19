fcdcd7b3daf0ce4e990c243d5de326f131ce30a0
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2024-07-19 10:20:57 +0530

    use shadowdom to avoid style interference

--- src/wiki-article.js
+++ src/wiki-article.js
@@ -1,19 +1,60 @@
 import './common.js';
 import { editIcon, historyIcon, talkIcon } from './icons.js'; './icons.js';
 
+const styleURL = new URL('./wiki-article.css', import.meta.url)
+const template = `
+<div class="wiki-article">
+    <picture class="thumbnail">
+        <source class="webp" type="image/webp">
+        <source class="png" type="image/png">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.webp" alt="" />
+    </picture>
+    <div class="title-desc-container">
+        <h2 class="title-header">
+        <a class="title" href="" target="_blank"></a>
+        </h2>
+        <p class="description">Loading...</p>
+        <p class="extract">Loading...</p>
+        <div class="meta">
+            <span class="logo">Wikipedia</span>
+            <a class="edit icon" href="" target="_blank">${editIcon}
+            </a>
+            <a class="history icon" href="" target="_blank">${historyIcon}</a>
+            <a class="talk icon" href="" target="_blank">${talkIcon}</a>
+        </div>
+    </div>
+</div>
+<style>
+@import url(${styleURL});
+</style>
+`
+
 class WikiArticle extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: 'open' }).innerHTML = template;
+    }
+
+    static get observedAttributes() {
+        return ['article', 'language'];
+    }
+
+    attributeChangedCallback(attrName, oldValue, newValue) {
+        if (newValue && newValue !== oldValue) {
+            this[attrName] = newValue;
+            this.fetchArticleData();
+        }
     }
 
     connectedCallback() {
         this.article = this.getAttribute('article');
-        this.language = this.getAttribute('language', 'en');
-        this.render();
+        this.language = this.getAttribute('language') || 'en';
         this.fetchArticleData();
     }
 
     async fetchArticleData() {
+        if (!this.article) return;
+        if (!this.language) return;
         const url = `https://${this.language}.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(this.article)}?redirect=true`;
         try {
             const response = await fetch(url);
@@ -21,7 +62,7 @@ class WikiArticle extends HTMLElement {
             const data = await response.json();
             this.updateArticle(data);
         } catch (error) {
-            this.querySelector('.description').innerText = 'Failed to load article.';
+            this.shadowRoot.querySelector('.description').innerText = 'Failed to load article.';
             console.error('Fetch error:', error);
         }
     }
@@ -30,53 +71,32 @@ class WikiArticle extends HTMLElement {
         const { title, description, extract, thumbnail, lang, dir, content_urls } = data;
         this.lang = lang | this.language;
         this.dir = dir;
-        this.querySelector('.title').innerText = title;
-        this.querySelector('.title').href = content_urls.desktop.page;
-        this.querySelector('.edit').href = content_urls.desktop.edit;
-        this.querySelector('.history').href = content_urls.desktop.revisions;
-        this.querySelector('.talk').href = content_urls.desktop.talk;
-        this.querySelector('.description').innerText = description;
-        this.querySelector('.extract').innerText = extract;
+        this.shadowRoot.querySelector('.title').innerText = title;
+        this.shadowRoot.querySelector('.title').href = content_urls.desktop.page;
+        this.shadowRoot.querySelector('.edit').href = content_urls.desktop.edit;
+        this.shadowRoot.querySelector('.history').href = content_urls.desktop.revisions;
+        this.shadowRoot.querySelector('.talk').href = content_urls.desktop.talk;
+        this.shadowRoot.querySelector('.description').innerText = description;
+        this.shadowRoot.querySelector('.extract').innerText = extract;
         if (thumbnail && thumbnail.source) {
-            if (thumbnail.source.includes('/wikipedia/')) {
+            if (!thumbnail.source.includes('/wikipedia/commons')) {
                 // not a commons image. local wiki image
-                this.querySelector('.wiki-article-thumbnail > .webp').remove();
-                this.querySelector('.wiki-article-thumbnail > .png').remove();
+                this.shadowRoot.querySelector('.thumbnail > .webp')?.remove();
+                this.shadowRoot.querySelector('.thumbnail > .png')?.remove();
             } else {
-                this.querySelector('.wiki-article-thumbnail > .webp').srcset = thumbnail.source.replace(
+                this.shadowRoot.querySelector('.thumbnail > .webp').srcset = thumbnail.source.replace(
                     /\.(jpg|png|jpeg)$/,
                     '.webp',
                 );
-                this.querySelector('.wiki-article-thumbnail > .png').srcset = thumbnail.source.replace(/\.(jpg|webp|jpeg)$/, '.png');
+                this.shadowRoot.querySelector('.thumbnail > .png').srcset = thumbnail.source.replace(/\.(jpg|webp|jpeg)$/, '.png');
             }
-            this.querySelector('.wiki-article-thumbnail > img').src = thumbnail.source;
+            this.shadowRoot.querySelector('.thumbnail > img').src = thumbnail.source;
         }
     }
 
     render() {
-        this.innerHTML = `
-              <div class="wiki-article">
-                  <picture class="wiki-article-thumbnail">
-                   <source class="webp" type="image/webp">
-                   <source class="png" type="image/png">
-                   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.webp" alt="" />
-                  </picture>
-                  <div class="wiki-article-title-desc-container">
-                    <h2 class="title-header">
-                    <a class="title" href="" target="_blank"></a>
-                    </h2>
-                    <p class="description">Loading...</p>
-                    <p class="extract">Loading...</p>
-                    <div class="wiki-article-meta">
-                        <span class="logo">Wikipedia</span>
-                        <a class="edit icon" href="" target="_blank">${editIcon}
-                        </a>
-                        <a class="history icon" href="" target="_blank">${historyIcon}</a>
-                        <a class="talk icon" href="" target="_blank">${talkIcon}</a>
+        this.shadowRoot.innerHTML = `
 
-                    </div>
-                  </div>
-              </div>
           `;
     }
 }
